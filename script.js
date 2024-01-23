d3f5f7c4ca2fbc0923ea123ad076146fc9ad8200
Esra <esra.ashoor.e@gmail.com>
2024-01-23 16:52:42 +0300

    adding comment to the js

--- script.js
+++ script.js
@@ -2,45 +2,57 @@ let foundCards = []
 let idColor = []
 let clickedCard = null
 let combosFound = []
+//a counter variable used to keep track of the number of matches made by the user.
 let matchCards = 0
 
+//select all elements in the document with the class name 'card' and store them in the cards variable.
 const cards = document.getElementsByClassName('card')
 
+//select all elements in the document with the class name 'color-hidden' and store them in the colorHidden variable.
 const colorHidden = document.getElementsByClassName('color-hidden')
 
-// iterate through cards, and for each card attach an event lisenter
-// for each card
-
-// add event listener
-
+// iterate through each element in the cards collection
 for (let i = 0; i < cards.length; i++) {
+  //for each card attach an event lisenter function for the 'click' event.
   cards[i].addEventListener('click', (event) => {
-    // if there are four cards in the array
-    //return
+    // if there are 2 cards with an array that has a length of 4(once the user has found the maximum number of cards this ensures that the user cannot select any additional cards )
+
     if (foundCards.length === 4) {
+      //return without executing any further code
       return
     }
 
-    // console.log(event.target)
+    //target: refers to the element(cards) that triggered the event.
+    //event.target: represent the specific card that the user clicked on.
+    //assigning event.target to the clickedCard variable
     const clickedCard = event.target
-    // remove the class of 'color-hidden' from the classList of the clickedCard
-
+    // remove the class of 'color-hidden' class from the clickedCard (this will show the card color)
     clickedCard.classList.remove('color-hidden')
 
+    // this save the clickared 'color' in a colorAttribute vriable
     const colorAttribute = clickedCard.getAttribute('color')
+
+    //used to disable another click events on the clickedCard element.
     clickedCard.style.pointerEvents = 'none'
 
-    //console.log('color', colorAttribute)
-    //foundCards.push(colorAttribute)
+    // this save the clickared 'id' in a idColor vriable
     const idColor = clickedCard.getAttribute('id')
+
+    //we are adding(or saving) the colorAttribute value as a new element at the end of the foundCards array.
     foundCards.push(colorAttribute)
+    //we are adding(or saving) the idColor value as a new element at the end of the foundCards array.(push the id)
     foundCards.push(idColor)
-    //
-    //get the elment id
-    // push the id
+
+    //(foundCards.length === 4 )This condition checks if the length of the foundCards array is equal to 4. This implies that the user has found and matched two cards.
+
+    //(foundCards[0] === foundCards[2])This condition checks if the value of the first card in the foundCards array is equal to the value of the third card. This suggests that the user has found a pair of matching cards.
+    //If both of these conditions are true, the code within the if statement {} block will be executed.
     if (foundCards.length === 4 && foundCards[0] === foundCards[2]) {
+      //This code increments the value(let matchCards = 0) stored in the matchCards variable by 1.
       matchCards++
+      //This code assigns an empty array ([]) to the foundCards variable. It clears the foundCards array, getting ready for the user to find the next set of matching cards.
       foundCards = []
+      //////////////
       // clickedCard.classList.remove('color-hidden')
       //openCards = openCards + 2
     } else if (foundCards.length === 4 && foundCards[1] !== foundCards[3]) {
@@ -69,54 +81,3 @@ for (let i = 0; i < cards.length; i++) {
 document.getElementById('play').addEventListener('click', function () {
   window.location.href = 'index.html'
 })
-
-/*if (cards[i].colorAttribute === cards[i].colorAttribute) {
-        const foundCards2 = cards[i].getAttribute('color')
-        foundCards++
-      }
-    if (cards[i].colorAttribute !== cards[i].colorAttribute) {
-      const foundCards3 = clickedCard.classList.add('color-hidden')
-      console.log('hhhhh')
-    }*/
-
-/*
-} else {
-  console.log("The symbols on the two cards do not match.");
-}
-
-if (combosFound === 6) {
-  alert('YOU WIN')
-}
-*/
-/*
-If it's the first card in a pair, remember its symbol.
-
-
-
-If it's the second card in a pair:
-
-
-
-Compare the symbols of the two cards.
-
-
-
-
-If they match:
-
-Keep the cards face-up.
-
-
-Increment the number of matched pairs.
-
-Check if all pairs have been matched.
-
-If all pairs have been matched, end the game.
-
-If they don't match:
-
-Wait for a short period.
-
-Flip both cards face-down.
-
-If the game ends, display a congratulatory message with the number of moves and elapsed time.*/
